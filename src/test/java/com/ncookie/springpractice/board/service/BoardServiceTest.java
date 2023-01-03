098b21a3b0eac59f2c13f74d3842816424c13afc
NCookies <swstar21c@gmail.com>
2023-01-03 21:56:52 +0900

    주석 추가

---
+++ src/test/java/com/ncookie/springpractice/board/service/BoardServiceTest.java
@@ -0,0 +1,33 @@
+package com.ncookie.springpractice.board.service;
+
+import org.junit.jupiter.api.Test;
+
+
+// 게시판 관련 서비스 테스트 코드
+class BoardServiceTest {
+
+    @Test
+    void savePost() {
+    }
+
+
+    @Test
+    void getBoardPageList() {
+    }
+
+    @Test
+    void getPageInfo() {
+    }
+
+    @Test
+    void getPost() {
+    }
+
+    @Test
+    void deletePost() {
+    }
+
+    @Test
+    void searchPosts() {
+    }
+}
